26c713e7a1b76a2f8534235b3964634863e4a3e3
Dongsheng Li <Lds010@outlook.com>
2021-11-22 11:28:30 +0800

    Initial commit

---
+++ src/main/java/com/evan/echartsbackend/EchartsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.evan.echartsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchartsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EchartsBackendApplication.class, args);
+    }
+
+}

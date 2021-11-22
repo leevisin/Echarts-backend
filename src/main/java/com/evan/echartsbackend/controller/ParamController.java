8f65c4d1d61eddb621122d258838b61ff13150a0
Dongsheng Li <Lds010@outlook.com>
2021-11-22 17:22:43 +0800

    实现同一组件的后端传参

--- src/main/java/com/evan/echartsbackend/controller/ParamController.java
+++ src/main/java/com/evan/echartsbackend/controller/ParamController.java
@@ -1,5 +1,6 @@
 package com.evan.echartsbackend.controller;
 
+import com.evan.echartsbackend.result.Option;
 import com.evan.echartsbackend.result.Result;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -15,9 +16,11 @@ public class ParamController {
     @CrossOrigin
     @PostMapping(value = "api/update")
     @ResponseBody
-    public Result update(@RequestBody Param requestParam){
+    public Option update(@RequestBody Param requestParam){
         String title = requestParam.getTitle();
+        Option returnOption = new Option();
+        returnOption.setTitle(title);
         System.out.println("title: " + title);
-        return new Result(200);
+        return returnOption;
     }
 }
---
+++ src/main/java/com/evan/echartsbackend/result/Option.java
@@ -0,0 +1,13 @@
+package com.evan.echartsbackend.result;
+
+public class Option {
+    String title;
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+}

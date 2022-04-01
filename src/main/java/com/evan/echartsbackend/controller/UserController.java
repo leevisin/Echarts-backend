0c51b98c049695f0be88ba7fccaf729f0ffa7fb0
Dongsheng Li <Lds010@outlook.com>
2022-04-02 01:10:48 +0800

    Read and Write Csv

--- src/main/java/com/evan/echartsbackend/controller/UserController.java
+++ src/main/java/com/evan/echartsbackend/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.evan.echartsbackend.controller;
 
+import com.csvreader.CsvReader;
+import com.csvreader.CsvWriter;
 import com.evan.echartsbackend.pojo.Chart;
 import com.evan.echartsbackend.result.Result;
 import com.evan.echartsbackend.result.Charts;
@@ -9,50 +11,66 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class UserController {
     @CrossOrigin
-    @PostMapping(value = "api/user")
+    @PostMapping(value = "api/addChart")
     @ResponseBody
     public List addChart(@RequestBody Chart chart) {
-        List list = new ArrayList();
-        Chart aChart = new Chart();
-        aChart.setData("option = {\n" +
-                "        title: {\n" +
-                "          text: 'Title Test',\n" +
-                "        },\n" +
-                "        tooltip: {},\n" +
-                "        xAxis: {\n" +
-                "          type: 'category',\n" +
-                "          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']\n" +
-                "        },\n" +
-                "        yAxis: {\n" +
-                "          type: 'value'\n" +
-                "        },\n" +
-                "        series: [\n" +
-                "          {\n" +
-                "            data: [150, 230, 224, 218, 135, 147, 260],\n" +
-                "            type: 'line'\n" +
-                "          }\n" +
-                "        ]\n" +
-                "      };");
-        aChart.setCover("https://static01.imgkr.com/temp/566c928d2ce14157963f828ba166e9aa.JPG");
-        aChart.setTitle(chart.getTitle());
-        Chart bChart = new Chart();
-        bChart.setData("456");
-        bChart.setCover("https://static01.imgkr.com/temp/566c928d2ce14157963f828ba166e9aa.JPG");
-        list.add(aChart);
-        list.add(bChart);
-        return list;
+        // Write Csv
+        String filePath = "src/charts.csv";
+        File f = new File(filePath);
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(f,true));
+            CsvWriter cwriter = new CsvWriter(writer,',');
+            cwriter.writeRecord(new String[]{chart.getCover(), chart.getTitle(), chart.getType(), chart.getData()},false);
+            cwriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return updateChart();
     }
 
-//    public Charts addChart(@RequestBody Chart chart) {
-//        System.out.println(chart.getData());
-//        Chart aChart = new Chart();
-//        aChart.setData(chart.getData());
-//        return new Charts(aChart);
-//    }
+    @CrossOrigin
+    @PostMapping(value = "api/updateChart")
+    @ResponseBody
+    public List updateChart() {
+        String filePath = "src/charts.csv";
+        // New list to return stored charts
+        List list = new ArrayList();
+        // Read Csv
+        try {
+            CsvReader csvReader = new CsvReader(filePath);
+
+            // 读表头
+            csvReader.readHeaders();
+
+            // 读内容
+            while (csvReader.readRecord()) {
+                // 读一整行
+                System.out.println(csvReader.getRawRecord());
+                // 读该行的某一列
+                System.out.println(csvReader.get("title"));
+                Chart aChart = new Chart();
+                aChart.setCover(csvReader.get("cover"));
+                aChart.setTitle(csvReader.get("title"));
+                aChart.setType(csvReader.get("type"));
+                aChart.setData(csvReader.get("data"));
+                list.add(aChart);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return list;
+    }
 }
